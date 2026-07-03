ef985fc9bc7f8d53343cd14371fba01aa9e85920
abbos <abbostemirov12321@gmail.com>
2026-07-03 11:01:35 +0500

    created chatcontroller

---
+++ src/main/java/dev/danvega/chatgpt/ChatController.java
@@ -0,0 +1,35 @@
+package dev.danvega.chatgpt;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ChatResponse chat(@RequestParam(required = false, defaultValue = "Write the code Spring AI") String userMsg) {
+
+        OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .withTemperature(0.0f)
+                .build();
+
+        return chatClient.prompt()
+                .user(userMsg)
+                .call()
+                .chatResponse();
+
+    }
+}
