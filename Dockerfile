05f823e7768890db65d056c1ea36cab7ef54cdd3
abbos <abbostemirov12321@gmail.com>
2026-07-06 15:41:44 +0500

    update

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
-# 1-bosqich: Loyihani yig'ish (Konteyner ichida Maven ishlaydi)
-FROM maven:3.9-eclipse-temurin-21-alpine AS build
-WORKDIR /app
-COPY pom.xml .
-COPY src ./src
-RUN mvn clean package -DskipTests
-
-# 2-bosqich: Faqat tayyor loyihani ishga tushirish (Minimal hajm)
 FROM eclipse-temurin:21-jdk-alpine
 VOLUME /tmp
-# Tayyor .jar faylni 1-bosqichdan nusxalab olamiz
-COPY --from=build /app/target/*.jar app.jar
+# Maven builddan chiqqan jar faylni konteynerga nusxalaymiz
+COPY target/*.jar app.jar
+# Dasturni ishga tushirish
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/dev/danvega/chatgpt/UserController.java
+++ src/main/java/dev/danvega/chatgpt/UserController.java
@@ -5,14 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 public class UserController {
 
 
+    private final ConcurrentHashMap<String, Integer> users = new ConcurrentHashMap<>();
+
+    public UserController() {
+        users.put("Ali", 20);
+        users.put("Vali", 22);
+    }
 
     @GetMapping("/users")
     public Map<String, Integer> getUsers() {
-        return Map.of("Ali", 20, "Toxir", 22);
+        return users;
     }
+
+
+
+
+
 }
